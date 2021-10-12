ee2cf26bef50ae44071d68e48eb179fcb6f62349
Aashish <aashesh.j@gmail.com>
2021-10-12 16:55:35 +0545

    Main Screen flash animation build

--- app/src/main/java/com/aashish/learnjava/MainActivity.java
+++ app/src/main/java/com/aashish/learnjava/MainActivity.java
@@ -4,50 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int mainScreen=5000;
+
+    //variable
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView logo,slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Animation
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image=findViewById(R.id.imageView);
+        logo=findViewById(R.id.textView107);
+        slogan=findViewById(R.id.textView108);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainActivity.this, mainScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
     }
 
-    public void chOne(View view) {
-        Intent intent = new Intent(this, chapterOne.class);
-        startActivity(intent);
-    }
-    public void chTwo(View view){
-        Intent intent = new Intent(this, chapterTwo.class);
-        startActivity(intent);
-    }
-    public void chThree(View view){
-        Intent intent = new Intent(this, chapterThree.class);
-        startActivity(intent);
-    }
-    public void chTen(View view){
-        Intent intent = new Intent(this, previousYearQuestionPaper.class);
-        startActivity(intent);
-    }
-    public void chFour(View view){
-        Intent intent = new Intent(this,chapterFour.class);
-        startActivity(intent);
-    }
-    public void chFive(View view){
-        Intent intent = new Intent(this, chapterFive.class);
-        startActivity(intent);
-    }
-    public void chSix(View view){
-        Intent intent= new Intent(this, chapterSix.class);
-        startActivity(intent);
-    }
-    public void chSeven(View view){
-        Intent intent= new Intent(this, chapterSeven.class);
-        startActivity(intent);
-    }
-    public void chNine(View view){
-        Intent intent=new Intent(this, chapterNine.class);
-        startActivity(intent);
-    }
+
 }
