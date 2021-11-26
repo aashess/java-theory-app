f7fc1964feae9a70ea669ce7030332bb40a32097
Aashish <aashesh.j@gmail.com>
2021-11-26 21:00:36 +0545

    mainScreen complete

---
+++ app/src/main/java/com/aashish/learnjava/subjectList.java
@@ -0,0 +1,14 @@
+package com.aashish.learnjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class subjectList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_list);
+    }
+}
